46ca8fb957eb998d2bf1bbfa28a366ef54c6a22b
Norbert Łukaszuk <norbert.lukaszuk@gmail.com>
2021-02-05 14:02:06 +0100

    dynanic routes

---
+++ pages/[project].js
@@ -0,0 +1,15 @@
+import React from "react";
+import Head from "next/head";
+const Project = () => {
+  return (
+    <div>
+      <Head>
+        <title>Create electronics blog | Home</title>
+        <link rel="icon" href="/ic.png" />
+      </Head>
+      <h3>This is project</h3>
+    </div>
+  );
+};
+
+export default Project;
