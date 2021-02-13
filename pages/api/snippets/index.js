7b053fa330f6038d2ba6405a5fd26a3e8c9f0f34
Norbert Łukaszuk <norbert.lukaszuk@gmail.com>
2021-02-13 14:10:34 +0100

    fetch all collections from firestoe attempt_1

--- pages/api/snippets/index.js
+++ pages/api/snippets/index.js
@@ -1,10 +1,28 @@
 import db from "../../../utils/db";
 export default async function handler(req, res) {
-  const snap = await db.collection("data/codeNotes/CSS").get();
+  const collections = await db
+    .collection("data")
+    .doc("codeNotes")
+    .listCollections();
 
-  const snippets = snap.docs.map((snap) => ({
-    id: snap.id,
-    ...snap.data(),
-  }));
-  res.status(200).json(snippets);
+  const collectionId = collections.map(
+    (collection) => collection._queryOptions.collectionId
+  );
+  const getAllCollections = async () => {
+     collectionId.forEach((doc) => {
+     let arr = 
+     await db.collection(doc)
+        .get()
+        .then((singleCollection) => arr.push(singleCollection));
+    });
+    return arr;
+  };
+  const allColletions = getAllCollections();
+  // const snap = await db.collection("data/codeNotes/CSS").get();
+
+  // const snippets = snap.docs.map((snap) => ({
+  //   id: snap.id,
+  //   ...snap.data(),
+  // }));
+  res.status(200).json(allColletions);
 }
