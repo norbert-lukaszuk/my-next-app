706568d87ca657ae7148602233b050837f31a223
Norbert Łukaszuk <norbert.lukaszuk@gmail.com>
2021-02-15 16:35:37 +0100

    get all of collections using .then in example.js

--- pages/api/example.js
+++ pages/api/example.js
@@ -7,19 +7,43 @@ admin.initializeApp({
 });
 
 const db = admin.firestore();
+let num = new Array();
+db.collection("data")
+  .doc("codeNotes")
+  .listCollections()
+  .then((resp) => resp.map((coll) => coll._queryOptions.collectionId))
+  .then((arr) =>
+    arr.forEach((doc) =>
+      db
+        .collection(`data/codeNotes/${doc}`)
+        .get()
+        .then((resp) =>
+          resp.docs.forEach((snipp) => console.log(snipp.data().lang))
+        )
+    )
+  );
+// async function getData() {
+//   try {
+//     const collections = await db
+//       .collection("data")
+//       .doc("codeNotes")
+//       .listCollections();
 
-async function getData() {
+//     const docs = collections.map((doc) => doc._queryOptions.collectionId);
+//     return docs;
+//   } catch (err) {
+//     console.log(err);
+//   }
+//   return docs;
+// }
+/* async function getDocs() {
   try {
-    const collections = await db
-      .collection("data")
-      .doc("codeNotes")
-      .listCollections();
-
-    const docs = collections.map((doc) => doc._queryOptions.collectionId);
-
-    console.log(docs);
+    const allDocs = await docs.forEach(async function (doc) {
+      const snipp = await db.collection(`data/codeNotes/${doc}`).get();
+      // console.log(snipp.docs);
+      return snipp;
+    });
   } catch (err) {
     console.log(err);
   }
-}
-getData();
+} */
