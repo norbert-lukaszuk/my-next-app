bc4e759bfebf9fc00e37a9ae6653c649103e870f
Norbert Łukaszuk <norbert.lukaszuk@gmail.com>
2021-02-18 16:41:54 +0100

    quick commit

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Link from "next/link";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+
 export default function Home() {
   console.log(process.env.NEXT_PUBLIC_FIREBASE_API_KEY);
   return (
--- pages/snippets/[id].js
+++ pages/snippets/[id].js
@@ -5,7 +5,7 @@ import { Box, Code, Container } from "@chakra-ui/react";
 import Prism from "prismjs";
 import "prismjs/plugins/line-numbers/prism-line-numbers";
 import "../../node_modules/prismjs/plugins/line-numbers/prism-line-numbers.css";
-
+import "prismjs/plugins/normalize-whitespace/prism-normalize-whitespace";
 export const getStaticPaths = async () => {
   const resp = await fetch("http://localhost:3000/api/snippets");
   const data = await resp.json();
@@ -28,26 +28,35 @@ const Project = ({ snippet }) => {
   useEffect(() => {
     Prism.highlightAll();
   }, []);
+  Prism.plugins.NormalizeWhitespace.setDefaults({
+    "remove-trailing": true,
+    "remove-indent": true,
+    "left-trim": true,
+    "right-trim": true,
+    "break-lines": 40,
+    "remove-initial-line-feed": true,
+    // "tabs-to-spaces": 4,
+    // "spaces-to-tabs": 4,
+  });
   return (
     <>
       <Head>
         <title>Create electronics blog | Project</title>
         <link rel="icon" href="/ic.png" />
       </Head>
-      {/* <Box m="15px" bg="gray.100" p={10}>
-          <Code
-            colorScheme="teal"
-            children={new Date(snippet.date).toISOString().slice(0, 10)}
-            p="5px"
-          ></Code>
-          <Code colorScheme="pink" children={snippet.lang} p="5px"></Code>
-          <Code colorScheme="cyan" children={snippet.code} p="5px"></Code>
-        </Box> */}
-      <pre className="line-numbers">
-        <code className={`language-${snippet.lang.toLowerCase()}`}>
-          <p> {snippet.code}</p>
-        </code>
-      </pre>
+      <Box m="15px" bg="gray.100" p={10}>
+        <Code
+          colorScheme="teal"
+          children={new Date(snippet.date).toISOString().slice(0, 10)}
+          p="5px"
+        ></Code>
+        <Code colorScheme="pink" children={snippet.lang} p="5px"></Code>
+        <pre className="line-numbers">
+          <code className={`language-${snippet.lang.toLowerCase()}`}>
+            <p> {snippet.code}</p>
+          </code>
+        </pre>
+      </Box>
     </>
   );
 };
